f1607fe7a726ee937d2701b8f341bf7b94a8d011
Jenna Lee <lee.jenna.n@gmail.com>
2016-10-09 13:13:40 -0400

    Added light purple color, changed spacing in top

--- mySite.js
+++ mySite.js
@@ -4,7 +4,7 @@ app.config(function($routeProvider) {
     $routeProvider
     
 	.when('/', {
-        templateUrl : 'pages/aboutMe.html'
+        templateUrl : 'pages/home.html'
     })
     
     .when('/portfolio', {
